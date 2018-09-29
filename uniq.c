84c61c5d9d966565df6b7d532ff75bb6f63ed925
Shang-Hung Tsai <st3127@nyu.edu>
2018-09-29 08:09:14 -0400

    extend uniq with flags

--- uniq.c
+++ uniq.c
@@ -7,60 +7,59 @@ char cur[512];
 char prev[512];
 
 void
-copy(char dest[], char src[], int len) {
-  int i;
-  for (i = 0; i <= len; i++) {
-    dest[i] = src[i];
-  }
-}
-
-int
-isduplicate(char a[], int alen, char b[], int blen) {
-  if (alen != blen) {
-    return 0;
-  }
-  int i;
-  for (i = 0; i <= alen; i++) {
-    if (a[i] != b[i]) {
-      return 0;
-    }
-  }
-  return 1;
-}
-
-void
-printline(char chars[], int len) {
-  int i;
-  for (i = 0; i < len; i++) {
-    printf(1, "%c", chars[i]);
-    if (chars[i] == '\n') {
-      break;
-    } 
-  }
-}
-
-void
-uniq(int fd, char *name)
+uniq(int fd, char *name, int iscount, int isduplicate, int iscase)
 {
   int i, n;
   int plen;
   int ci;
+  int count, isfirst;
 
   ci = plen = 0;
+  count = 0;
+  isfirst = 1;
   while((n = read(fd, buf, sizeof(buf))) > 0){
     for(i=0; i<n; i++){
       cur[ci] = buf[i];
+      if (iscase > 0 && cur[ci] >= 'A' && cur[ci] <= 'Z') {
+        cur[ci] = cur[ci] - 'A' + 'a';
+      }
       ci++;
       if(buf[i] == '\n') {
-	if (!isduplicate(cur, ci, prev, plen)) {
-          printline(cur, ci);
+	if (strcmp(cur, prev)) {  // not equal
+          if (isfirst) {
+            isfirst = 0;
+ 	  } else if (iscount) {
+            printf(1, "%d %s", count, prev);
+          } else if (isduplicate) {
+            if (count > 1) {
+              printf(1, "%s", prev);
+            }
+          } else {
+            printf(1, "%s", prev);
+          }
           plen = ci;
-          copy(prev, cur, plen);
-	}
+          memmove(prev, cur, plen);
+          count = 1;
+	} else {
+          count++;
+        }
         ci = 0;
       }
     }
   }
+
+  if (isfirst) {
+    isfirst = 0;
+  } else if (iscount) {
+    printf(1, "%d %s", count, prev);
+  } else if (isduplicate) {
+    if (count > 1) {
+      printf(1, "%s", prev);
+    }
+  } else {
+    printf(1, "%s", prev);
+  }
+
   if(n < 0){
     printf(1, "uniq: read error\n");
     exit();
@@ -73,16 +72,34 @@ main(int argc, char *argv[])
   int fd, i;
 
   if(argc <= 1){
-    uniq(0, "");
+    uniq(0, "", 0, 0, 0);
     exit();
   }
 
-  for(i = 1; i < argc; i++){
+  int iscount, isduplicate, iscase;
+  for (i = 1; i < argc && argv[i][0] == '-'; i++) {
+    switch (argv[i][1]) {
+      case 'c':
+        iscount = 1;
+        break;
+      case 'd':
+        isduplicate = 1;
+        break;
+      case 'i':
+        iscase = 1;
+        break;
+      default:
+        printf(1, "uniq: invalid flag %s", argv[i]);
+        exit();
+    }
+  } 
+
+  for(; i < argc; i++){
     if((fd = open(argv[i], 0)) < 0){
       printf(1, "uniq: cannot open %s\n", argv[i]);
       exit();
     }
-    uniq(fd, argv[i]);
+    uniq(fd, argv[i], iscount, isduplicate, iscase);
     close(fd);
   }
   exit();
